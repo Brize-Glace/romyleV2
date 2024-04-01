c69adae58f4e8a00ec1935be798bf8f12a6a1e25
Brize Glace <viktorovitchromain@gmail.com>
2024-04-01 22:48:07 +0200

    Update links and styles in about.html, songs.css, style.css, and script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,21 +16,33 @@ menuHamburger.addEventListener('click', () => {
     }
 });
 
-const songsButtons = document.querySelector('.myMusicBtn')
-const selectionedPlaylistButton = document.querySelector('.selectionedPlaylistsBtn')
-const songsDiv = document.querySelector('.mysongs')
-const playlistsDiv = document.querySelector('.selectionedPlaylists')
+const songsButtons = document.querySelector(".myMusicBtn");
+const selectionedPlaylistButton = document.querySelector(".selectionedPlaylistsBtn");
+const songsDiv = document.querySelector(".mysongs");
+const playlistsDiv = document.querySelector(".selectionedPlaylists");
+const transitionDuration = 200; 
 
 songsButtons.addEventListener('click', () => {
-        songsButtons.classList.add('selected');
-        selectionedPlaylistButton.classList.remove('selected');
-        songsDiv.style.display = 'block';
-        playlistsDiv.style.display = 'none';
-})
+    songsButtons.classList.add('selected');
+    selectionedPlaylistButton.classList.remove('selected');
+    songsDiv.style.display = 'block';
+    playlistsDiv.style.display = 'none';
+    songsDiv.style.opacity = '0'; 
+    songsDiv.style.transition = `opacity ${transitionDuration}ms ease-in-out`; 
+    setTimeout(() => {
+        songsDiv.style.opacity = '1'; 
+    }, transitionDuration);
+});
+
 selectionedPlaylistButton.addEventListener('click', () => {
     selectionedPlaylistButton.classList.add('selected');
     songsButtons.classList.remove('selected');
-        songsDiv.style.display = 'none';
-        playlistsDiv.style.display = 'block';
+    songsDiv.style.display = 'none';
+    playlistsDiv.style.display = 'block';
+    playlistsDiv.style.opacity = '0';
+    playlistsDiv.style.transition = `opacity ${transitionDuration}ms ease-in-out`;
+    setTimeout(() => {
+        playlistsDiv.style.opacity = '1'; 
+    }, transitionDuration);
 });
-      
+ 
