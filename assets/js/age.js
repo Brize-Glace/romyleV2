3ce6bf107bbee0d5eb67c2cebe97fec08a4c52fe
Brize Glace <viktorovitchromain@gmail.com>
2024-04-30 19:36:48 +0200

    Update about.html, songs.css, age.js, contact.html, index.html, and songs.html

---
+++ assets/js/age.js
@@ -0,0 +1,11 @@
+const currentDate = new Date();
+const birthDate = new Date('2006-04-19');
+const ageElement = document.getElementById('spanAge');
+
+if (currentDate.getMonth() === birthDate.getMonth() && currentDate.getDate() >= birthDate.getDate()) {
+    const age = currentDate.getFullYear() - birthDate.getFullYear();
+    ageElement.textContent = age;
+} else {
+    const age = currentDate.getFullYear() - birthDate.getFullYear() - 1;
+    ageElement.textContent = age;
+}
